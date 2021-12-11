f6b9fcd717c0b5f90992d070bc92648c6f5ba5b0
krunicnina <95870738+krunicnina@users.noreply.github.com>
2021-12-11 17:53:50 +0100

    Gotova forma, html deo. Fale css, js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+  document.getElementById("submit").addEventListener("click", funkcija);
+
+  function funkcija() {
+    alert ("Uspesno ste se registrovali!");
+  }
+  document.getElementsById("reset").addEventListener("click", funkcija2);
+
+  function funkcija2() {
+    alert ("Forma se restartuje");
+  }
+
+ 
