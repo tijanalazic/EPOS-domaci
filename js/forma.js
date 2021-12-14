f77f59f2def0a88b8d7d76e713015dbf0409d8c3
krunicnina <95870738+krunicnina@users.noreply.github.com>
2021-12-14 23:26:38 +0100

    forma dogadjaji

--- js/forma.js
+++ js/forma.js
@@ -1,11 +1,34 @@
-document.getElementById("submit").addEventListener("click", funkcija);
-
-  function funkcija() {
-   alert ("Uspesno ste se registrovali!");
-  } 
+function funkcija() {
+  let text;
+  let osoba = prompt("Unesite Vaše ime:", "Ivana Petrović");
+  if (osoba == null || osoba == "") {
+    text = "Niste uneli svoje ime.";
+  } else {
+    text = "Zdravo " + osoba + "! U toku je mesec besplatnih preventivnih pregleda!";
+  }
+  document.getElementById("poruka").innerHTML = text;
+}
+  $("#submit").click(function() {
+    alert("Uspešno ste se registrovali!");
+ });
  
   document.getElementsById("reset").addEventListener("click", funkcija2);
 
   function funkcija2() {
     return confirm("Forma je uspesno resetovana");
   }
+/*   $(document).ready(function(){
+    $("#submit").click(function(){
+      alert("Uneta vrednost: " + $("#inputID").val());
+    });
+  }); */
+  function potvrda()  {
+
+    var rezultat = confirm("Do you want to continue?");
+
+    if(rezultat)  {
+        alert("Nastavite sa popunjavanjem forme!");
+    } else {
+        alert("Doviđenja!");
+    }
+}
---
+++ js/onama.js
@@ -0,0 +1,10 @@
+/* $(document).ready(function() {
+    $("p").dblclick(function() {
+        $("p").hide();
+    });
+});  */
+
+/* $(document).click(function(){
+        $("drugiDiv").toggle( "bounce", { times: 3 }, "slow" );
+    });
+     */
