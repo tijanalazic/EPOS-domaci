84be1de66ce7e9353958d0cbaf4be2d0ef319cc6
krunicnina <95870738+krunicnina@users.noreply.github.com>
2021-12-18 18:29:05 +0100

    gotova responzivna forma

--- js/forma.js
+++ js/forma.js
@@ -8,27 +8,45 @@ function funkcija() {
   }
   document.getElementById("poruka").innerHTML = text;
 }
-  $("#submit").click(function() {
-    alert("Uspešno ste se registrovali!");
- });
- 
-  document.getElementsById("reset").addEventListener("click", funkcija2);
+$("#submit").click(function() {
+  alert("Uspešno ste se registrovali!");
+});
+$("#reset").click(function() {
+  alert("Forma je uspešno resetovana");
+});
+function potvrda()  {
 
-  function funkcija2() {
-    return confirm("Forma je uspesno resetovana");
+  var rezultat = confirm("Da li želite da nastavite?");
+
+  if(rezultat)  {
+      alert("Nastavite sa popunjavanjem forme!");
+  } else {
+      alert("Doviđenja!");
   }
-/*   $(document).ready(function(){
-    $("#submit").click(function(){
-      alert("Uneta vrednost: " + $("#inputID").val());
-    });
-  }); */
-  function potvrda()  {
-
-    var rezultat = confirm("Do you want to continue?");
-
-    if(rezultat)  {
-        alert("Nastavite sa popunjavanjem forme!");
-    } else {
-        alert("Doviđenja!");
-    }
 }
+/* $("input:checkbox").on('click', function() {
+  
+  var $checkbox = $(this);
+  if ($checkbox.is(":checked")) {
+    
+    var group = "input:checkbox[name='" + $checkbox.attr("name") + "']";
+    
+    $(group).prop("checked", false);
+    $checkbox.prop("checked", true);
+  } else {
+    $checkbox.prop("checked", false);
+  }
+}); */
+$("input:checkbox").on('click', function() {
+  
+  var $cekiraj = $(this);
+  if ($cekiraj.is(":checked")) {
+    
+    var group = "input:checkbox[name='" + $cekiraj.attr("name") + "']";
+    
+    $(group).prop("checked", false);
+    $cekiraj.prop("checked", true);
+  } else {
+    $cekiraj.prop("checked", false);
+  }
+});
